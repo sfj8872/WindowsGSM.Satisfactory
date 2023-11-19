5c2b09592a43837b073240c6796e213048d570c3
sfj8872 <80215353+sfj8872@users.noreply.github.com>
2023-11-19 01:09:55 -0500

    Update Satisfactory.cs
    updated for use with new UE5 Engine 
    
    Make sure to start a new server Save first as some old UE4 Saves may not allow servers to start and the server will crash

--- Satisfactory.cs/Satisfactory.cs
+++ Satisfactory.cs/Satisfactory.cs
@@ -35,7 +35,7 @@ namespace WindowsGSM.Plugins
 
 
         // - Game server Fixed variables
-        public override string StartPath => @"Engine\Binaries\Win64\UE4Server-Win64-Shipping.exe"; // Game server start path
+        public override string StartPath => @"Engine\Binaries\Win64\UnrealServer-Win64-Shipping.exe"; // Game server start path
         public string FullName = "Satisfactory Dedicated Server Early"; // Game server FullName
         public bool AllowsEmbedConsole = true;  // Does this server support output redirect?
         public int PortIncrements = 1; // This tells WindowsGSM how many ports should skip after installation
